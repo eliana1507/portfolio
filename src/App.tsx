e933435d4103139fac758726b96717d67e8a23ca
Andrea-Agosta <andrea.agosta85@gmail.com>
2023-04-24 17:09:12 +0200

    create home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Home } from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App box">
+      <Home />
       <h1 className="text-orange-500">Welcome to my Portfolio!</h1>
     </div>
   );
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,16 @@
+import './Home.css';
+
+export const Home = () => {
+  return (
+    <>
+      <section className="box">
+        <article className="flex flex-col align-middle items-center">
+          <img className='w-64 rounded-full my-10' src="https://media.licdn.com/dms/image/C4D03AQF-1SEMxyfuxA/profile-displayphoto-shrink_800_800/0/1621088601599?e=2147483647&v=beta&t=p7SDprSzsvN9SJaNv5qTTdd6AJud_sjGSKDUVEPqZks" alt="profile" />
+          <h1 className='title text-5xl'>I'm Eliana</h1>
+          <h4 className='subtitle text-xl'>Senior Java Developer & Architect</h4>
+          <p className='p-6 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora pariatur totam illum veniam! Hic commodi aut et, aliquid similique aspernatur obcaecati? Iusto cupiditate aspernatur quisquam illo dolorem ad ullam, aliquam veniam expedita deleniti sapiente iste rerum minima enim debitis officia nihil voluptas perspiciatis similique fugit aut nisi? Optio, odio doloremque.</p>
+        </article>
+      </section>
+    </>
+  )
+}  
