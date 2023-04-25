eab1ea556561e884ad749f02c6cc1e2276042121
Andrea-Agosta <andrea.agosta85@gmail.com>
2023-04-25 12:16:36 +0200

    create about component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
 import './App.css';
+import About from './components/About/About';
 import { Home } from './components/Home/Home';
 
 function App() {
   return (
     <div className="App box">
       <Home />
+      <About />
     </div>
   );
 }
---
+++ src/components/About/About.tsx
@@ -0,0 +1,14 @@
+const About = () => {
+  return (
+    <>
+      <section className="my-10">
+        <article className="text-center p-5">
+          <h2 className="title">About me</h2>
+          <p className="py-5"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit fugiat veniam velit asperiores, omnis, iure in quas alias sequi sapiente quisquam magnam consequatur similique earum. Consequuntur nobis ipsum eum. Omnis?</p>
+        </article>
+      </section>
+    </>
+  )
+}
+
+export default About
