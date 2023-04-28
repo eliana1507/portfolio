43fd7a5c0f779fbfdc56610d112acf71e27b4d66
Andrea-Agosta <andrea.agosta85@gmail.com>
2023-04-28 16:11:49 +0200

    refactoring and create a new component to show the work history in tablet/desktop view

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import About from './components/About/About';
 import { Home } from './components/Home/Home';
 import { WorksHistory } from './components/WorksHistory/WorksHistory';
+import { WorksHistoryMobile } from './components/WorksHistory/WorksHistoryMobile/WorksHistoryMobile';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Home />
       </div>
       <About />
+      <WorksHistoryMobile />
       <WorksHistory />
     </>
   );
