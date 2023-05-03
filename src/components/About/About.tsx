3c6dff38d97cc18d0d95ad8a1e7e4fe5efda2d58
Andrea-Agosta <andrea.agosta85@gmail.com>
2023-05-03 12:01:40 +0200

    update style

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <>
       <section className="my-10">
-        <article className="text-center p-5">
+        <article className="text-center p-5 md:px-16 lg:px-28">
           <h2 className="title text-5xl">About me</h2>
           <p className="py-5"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit fugiat veniam velit asperiores, omnis, iure in quas alias sequi sapiente quisquam magnam consequatur similique earum. Consequuntur nobis ipsum eum. Omnis?</p>
         </article>
